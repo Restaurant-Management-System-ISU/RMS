c5c2be36b0cd63e77fe8264232c2fc7cd22f2804
Sai Vineela Madamanchi <133285349+vineela1511@users.noreply.github.com>
2023-10-17 21:04:52 -0500

    created_signup_login

---
+++ src/main/java/com/rms/app/controller/RMSController.java
@@ -0,0 +1,80 @@
+package com.rms.app.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.rms.app.model.User;
+import com.rms.app.service.UserService;
+
+
+@Controller
+public class RMSController {
+	
+	@Autowired
+	private UserService userService;
+	
+	
+	@GetMapping("/")
+	public String getHome(Model model) {
+		return "index";
+	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+
+		User user = new User();
+		model.addAttribute("user", user);
+		return "home/register";
+	}
+
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute("user") User user,Model model) {
+		System.out.println("save===user");
+		
+	
+
+		User existingUser = userService.findUser(user.getEmail());
+
+		if (existingUser != null) {
+			model.addAttribute("errormsg", "Email already exists");
+			return "home/error";
+		}
+
+		User existingUsername = userService.findUserByUsername(user.getUsername());
+
+		if (existingUsername != null) {
+			model.addAttribute("errormsg", "Username already exists");
+			return "home/error";
+		}
+
+		int output = userService.saveUser(user);
+		
+		if (output > 0) {
+			return "redirect:/login";
+		} else {
+			model.addAttribute("errormsg", "Account creation failed");
+			return "home/error";
+		}
+	}
+	
+
+
+	
+
+}
