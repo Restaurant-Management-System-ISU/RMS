8a10b7250edee38424d5270b0e427c23291bc239
shreyaveeramaneni <shreyaveeramaneni@gmail.com>
2023-11-06 02:01:25 -0600

    Created Add to cart, Cart and Remove from cart
    Backend for cart

--- src/main/java/com/rms/app/controllers/CustomerController.java
+++ src/main/java/com/rms/app/controllers/CustomerController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.rms.app.model.Cart;
 import com.rms.app.model.Menu;
 import com.rms.app.model.Tables;
 import com.rms.app.model.User;
@@ -100,6 +101,112 @@ public class CustomerController {
 			return "home/error";
 	}
 
+		@PostMapping("/addToCart/{id}")
+	public String addToCart(Model model, HttpSession session, @PathVariable(name="id") Long id, @RequestParam("quantity") String quantity) {
+		
+		
+		@SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+		if(messages == null) {
+			model.addAttribute("errormsg", "Session Expired. Please Login Again");
+			return "home/error";
+		}
+		Menu menu = adminService.getMenuById(id);
+		Cart cart = new Cart();
+		
+		cart.setCategory(menu.getCategory());
+		cart.setCuisine(menu.getCuisine());
+		cart.setCustomerEmail(messages.get(0));
+		cart.setDescription(menu.getDescription());
+		cart.setName(menu.getName());
+		cart.setType(menu.getType());
+		cart.setVegOrNonVeg(menu.getVegOrNonVeg());
+		cart.setQuantity(quantity);
+		cart.setPrice(menu.getPrice());
+		int totalPrice = Integer.parseInt(menu.getPrice())* Integer.parseInt(quantity);
+		cart.setTotalPrice(String.valueOf(totalPrice));
+		
+		userService.addToCart(cart);
+		
+        model.addAttribute("sessionMessages", messages);
+		
+		return "redirect:/cart";
+	}
+	
+	@GetMapping("/cart")
+	public String cart(Model model, HttpSession session) {
+		
+		
+		@SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+		if(messages == null) {
+			model.addAttribute("errormsg", "Session Expired. Please Login Again");
+			return "home/error";
+		}
+		User userdata = userService.findUser(messages.get(0));
+		List<Cart> carts = userService.getUserCart(userdata.getEmail());
+		model.addAttribute("carts", carts);
+		
+		int finalPrice = 0;
+		for(int i=0;i<carts.size();i++) {
+			finalPrice = finalPrice + Integer.parseInt(carts.get(i).getTotalPrice());
+		}
+		
+		if(finalPrice > 0) {
+			model.addAttribute("finalprice", finalPrice);
+		}
+		else {
+			model.addAttribute("finalprice", "0");
+		}
+		
+		
+		int cartSize = carts.size();
+		if(cartSize > 0) {
+			model.addAttribute("flag", 1);
+		}
+		else {
+			model.addAttribute("flag", 0);
+		}
+		
+		
+		model.addAttribute("cartsize", cartSize);
+		System.out.println(carts.size()+"------");
+		model.addAttribute("role", userdata.getUsertype());
+		
+		return "customer/cart";
+		
+	}
+	
+
+	@PostMapping("/removeFromCart/{id}")
+	public String removeFromCart(@PathVariable(name="id") Long id)
+	{
+		userService.deleteFromCart(id);
+		
+		return "redirect:/cart";
+	}
+	
+	@GetMapping("/reservetable")
+	public String getCustomerProfile(@ModelAttribute("table") Tables table, Model model, HttpSession session)
+	{
+		@SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("MY_SESSION_MESSAGES");
+
+		if(messages == null) {
+			model.addAttribute("errormsg", "Session Expired. Please Login Again");
+			return "home/error";
+		}
+        model.addAttribute("sessionMessages", messages);
+        
+		User userModel = userService.getUserByEmail(messages.get(0));
+		 
+		Tables tableModel = new Tables();
+			
+	        
+	    model.addAttribute("table", tableModel);
+
+		return "customer/reservetable";
+	}
 
 
 	@GetMapping("/reservetable")
