831ef54618b0732dc25d833758121422ee9c734e
Shreya Veeramaneni <shreyaveeramaneni@gmail.com>
2023-12-07 20:49:24 -0600

    updated generate bills and make payment

--- src/main/java/com/rms/app/service/StaffService.java
+++ src/main/java/com/rms/app/service/StaffService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.rms.app.model.Bill;
 import com.rms.app.model.Menu;
+import com.rms.app.model.Notification;
 import com.rms.app.model.Order;
+import com.rms.app.model.Review;
 import com.rms.app.model.Staff;
 import com.rms.app.model.Stock;
 import com.rms.app.model.Tables;
@@ -29,7 +31,7 @@ public interface StaffService {
 	void confirmOrder(Long id);
 
 	void cancellOrder(Long id);
-	
+
 	void updateOrderStatus(Long id, String status);
 
 	List<Order> getConfirmedOrders();
@@ -42,11 +44,20 @@ public interface StaffService {
 
 	List<Bill> getBill(String tableName);
 
-	Menu getMenuById(String menuItem);
+	List<Menu> getMenuById(String menuItem);
 
 	void saveAssign(Bill bill);
 
-	
+	void completePayment(String tableName);
+
+	List<Notification> getAllNotifications();
+
+	Tables getTableById(Long id);
+
+	Order getOrderById(Long id);
+
+	List<Review> getCustomerReviews();
+
 
 
 
