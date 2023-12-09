97db0eb0115357417e03b5e825ad79e5e38f8388
Sai Vineela Madamanchi <saivineela.madamanchi@gmail.com>
2023-12-08 23:46:54 -0600

    updated tests
    Co-Authored-By: srilekhanaidu <58164165+srilekhanaidu@users.noreply.github.com>
    Co-Authored-By: Shreya Veeramaneni  <148269949+shreya366@users.noreply.github.com>
    Co-Authored-By: SairahulBaddapuram <144637965+SairahulBaddapuram@users.noreply.github.com>
    Co-Authored-By: ShanmukCh <148307242+ShanmukCh@users.noreply.github.com>

--- src/test/java/com/rms/app/service/UserServiceTest.java
+++ src/test/java/com/rms/app/service/UserServiceTest.java
@@ -16,12 +16,20 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DataIntegrityViolationException;
 
+import com.rms.app.dao.CartRepo;
 import com.rms.app.dao.MenuRepo;
 import com.rms.app.dao.OrderRepo;
+import com.rms.app.dao.ReviewRepo;
 import com.rms.app.dao.StaffRepo;
+import com.rms.app.dao.TablesRepo;
+import com.rms.app.dao.TicketRepo;
 import com.rms.app.dao.UserRepo;
+import com.rms.app.model.Cart;
 import com.rms.app.model.Menu;
 import com.rms.app.model.Order;
+import com.rms.app.model.Review;
+import com.rms.app.model.Tables;
+import com.rms.app.model.Ticket;
 import com.rms.app.model.User;
 
 import java.util.ArrayList;
@@ -31,63 +39,370 @@ import java.util.List;
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
-    @InjectMocks
+	@InjectMocks
     private UserServiceImpl userService;
-
-    @Mock
+	@Mock
     private UserRepo userRepo;
 
     @Mock
-    private OrderRepo orderRepo;
+    private CartRepo cartRepo;
+    
+    @Mock
+    private MenuRepo menuRepo;
 
+    
+    @Mock
+    private TablesRepo tableRepo;
+    
+    @Mock
+    private OrderRepo orderRepo;
+    
+    @Mock
+    private ReviewRepo reviewRepo;
+    
+    @Mock
+    private TicketRepo ticketRepo;
+    
+    
     @Test
-    public void getAllOrders() {
+    public void getAllMenus() {
+
+        Menu menu = new Menu();
+        
+        menu.setCategory("Starters");
+        menu.setCuisine("indian");
+        menu.setDescription("nicely chopped cabbage with deeply fried manchurian");
+        menu.setName("Veg Manchurian");
+        menu.setPrice("200");
+        menu.setType("type");
+        menu.setVegOrNonVeg("Veg");
+        menu.setSeason("normal");
+        
+        
+        List<Menu> menus = new ArrayList<>();
+        menus.add(menu);
+        
+        when(menuRepo.findAll()).thenReturn(menus);
+
+        assertEquals(menus, userService.getAllMenu());
+
+    }
+    
+    @Test
+    public void getEmptyMenus() {
+
+        Menu menu = new Menu();
+        
+        menu.setCategory("Starters");
+        menu.setCuisine("indian");
+        menu.setDescription("nicely chopped cabbage with deeply fried manchurian");
+        menu.setName("Veg Manchurian");
+        menu.setPrice("200");
+        menu.setType("type");
+        menu.setVegOrNonVeg("Veg");
+        menu.setSeason("normal");
+        
+        
+        List<Menu> menus = new ArrayList<>();
+        
+        when(menuRepo.findAll()).thenReturn(menus);
+
+        assertEquals(0, userService.getAllMenu().size());
+
+    }
+    
+    @Test
+    public void save() {
+
+        Cart cart = new Cart();
+        
+        cart.setCategory("Starters");
+        cart.setCuisine("indian");
+        cart.setDescription("nicely chopped cabbage with deeply fried manchurian");
+        cart.setName("Veg Manchurian");
+        cart.setPrice("200");
+        cart.setType("type");
+        cart.setVegOrNonVeg("Veg");
+        cart.setCustomerEmail("customer@gmail.com");
+        
+        
+        List<Cart> carts = new ArrayList<>();
+        carts.add(cart);
+        
+        when(cartRepo.findAll()).thenReturn(carts);
+
+        assertEquals(carts, userService.getUserCart("customer@gmail.com"));
+
+    }
+    
+    @Test
+    public void getUserCart() {
+
+        Cart cart = new Cart();
+        
+        cart.setCategory("Starters");
+        cart.setCuisine("indian");
+        cart.setDescription("nicely chopped cabbage with deeply fried manchurian");
+        cart.setName("Veg Manchurian");
+        cart.setPrice("200");
+        cart.setType("type");
+        cart.setVegOrNonVeg("Veg");
+        cart.setCustomerEmail("customer@gmail.com");
+        
+        
+        List<Cart> carts = new ArrayList<>();
+        carts.add(cart);
+        
+        when(cartRepo.findAll()).thenReturn(carts);
+
+        assertEquals(0, userService.getUserCart("custmer@gmail.com").size());
+
+    }
+    
+    @Test
+    public void deleteFromCart() {
+
+        Cart cart = new Cart();
+        
+        cart.setCategory("Starters");
+        cart.setCuisine("indian");
+        cart.setDescription("nicely chopped cabbage with deeply fried manchurian");
+        cart.setName("Veg Manchurian");
+        cart.setPrice("200");
+        cart.setType("type");
+        cart.setVegOrNonVeg("Veg");
+        cart.setCustomerEmail("customer@gmail.com");
+        
+        
+        List<Cart> carts = new ArrayList<>();
+        carts.add(cart);
+        
+        cartRepo.delete(cart);
+        
+        when(cartRepo.findAll()).thenReturn(carts);
+
+        assertEquals(0, userService.getUserCart("custmer@gmail.com").size());
+
+    }
+    
+    @Test
+    public void saveReservation() {
+
+        Tables table = new Tables();
+        
+        table.setCustomerEmail("customer@gmail.com");
+        table.setDatetime("1-12-2023::10:10");
+        table.setName("t1");
+        table.setNoOfPersons("4");
+        
+        tableRepo.save(table);
+        
+        List<Tables> tables = new ArrayList<>();
+        tables.add(table);
+        
+        
+        when(tableRepo.findAll()).thenReturn(tables);
+
+        assertEquals(1, tableRepo.findAll().size());
+
+    }
+    
+    @Test
+    public void saveNoReservation() {
+
+        Tables table = new Tables();
+        
+        table.setCustomerEmail("customer@gmail.com");
+        table.setDatetime("1-12-2023::10:10");
+        table.setName("t1");
+        table.setNoOfPersons("4");
+        
+        
+        List<Tables> tables = new ArrayList<>();
+       
+        
+        
+        when(tableRepo.findAll()).thenReturn(tables);
+
+        assertEquals(0, tableRepo.findAll().size());
+
+    }
+    
+    @Test
+    public void getCustomerOrders() {
 
         Order order = new Order();
         
-       order.setCardName("cardname");
-       order.setCardNumber("1234");
-       order.setCvv("123");
-       order.setEmail("customer@gmail.com");
-       order.setFinalBill("1234");
+        order.setCardName("cardName");
+        order.setCardNumber("1234567890");
+        order.setCvv("321");
+        order.setEmail("customer@gmail.com");
+        order.setFinalBill("1230");
+        order.setName("Pizza");
+        order.setPrice("123");
+        order.setQuantity("10");
+        order.setTotalCost("1230");
+        order.setStatus("ordered");
         
         
         List<Order> orders = new ArrayList<>();
-        orders.add(order);
+        orders.add(order);       
+        
         
         when(orderRepo.findAll()).thenReturn(orders);
 
-        assertEquals(orders, userService.getCustomerOrders("customer@gmail.com"));
+        assertEquals(1, orderRepo.findAll().size());
 
     }
     
     @Test
-    public void getAllUsers() {
+    public void getNoCustomerOrders() {
 
-    	 User user = new User();
-    	 
-    	 user.setEmail("user@gmail.com");
-    	 user.setLastname("lastname");
-    	 user.setFirstname("firstname");
-    	 user.setMobileNumber("1234567890");
-    	 user.setCity("city");
-    	 user.setDoorNo("12");
-    	 user.setUsername("username");
-         
-         
-         List<User> users = new ArrayList<>();
-         users.add(user);
-         
-         when(userRepo.findAll()).thenReturn(users);
+        Order order = new Order();
+        
+        order.setCardName("cardName");
+        order.setCardNumber("1234567890");
+        order.setCvv("321");
+        order.setEmail("customer@gmail.com");
+        order.setFinalBill("1230");
+        order.setName("Pizza");
+        order.setPrice("123");
+        order.setQuantity("10");
+        order.setTotalCost("1230");
+        order.setStatus("ordered");
+        
+        
+        List<Order> orders = new ArrayList<>();
+        
+        
+        when(orderRepo.findAll()).thenReturn(orders);
 
-         assertEquals(users, userService.getAllUsers());
+        assertEquals(0, orderRepo.findAll().size());
 
     }
     
+    @Test
+    public void deleteOrder() {
+
+        Order order = new Order();
+        
+        order.setCardName("cardName");
+        order.setCardNumber("1234567890");
+        order.setCvv("321");
+        order.setEmail("customer@gmail.com");
+        order.setFinalBill("1230");
+        order.setName("Pizza");
+        order.setPrice("123");
+        order.setQuantity("10");
+        order.setTotalCost("1230");
+        order.setStatus("ordered");
+        order.setId(1L);
+        orderRepo.save(order);
+        orderRepo.delete(order);
+        
+        
+        List<Order> orders = new ArrayList<>();
+        
+        
+        when(orderRepo.findAll()).thenReturn(orders);
+
+        assertEquals(0, orderRepo.findAll().size());
+
+    }
     
+    @Test
+    public void saveReview() {
+
+        Review review = new Review();
+        		
+        review.setCustomerEmail("customer@gmail.com");
+        review.setFeedback("good");
+        review.setId(1L);
+        review.setRating("5");
+        review.setOrderId("1");
+        
+        reviewRepo.save(review);
+        
+        
+        
+        
+        List<Review> reviews = new ArrayList<>();
+        reviews.add(review);
+        
+        
+        when(reviewRepo.findAll()).thenReturn(reviews);
+
+        assertEquals(1, reviewRepo.findAll().size());
+
+    }
     
+    @Test
+    public void saveNoreview() {
+
+        Review review = new Review();
+        		
+        review.setCustomerEmail("customer@gmail.com");
+        review.setFeedback("good");
+        review.setId(1L);
+        review.setRating("5");
+        review.setOrderId("1");
+        
+        
+        
+        
+        
+        List<Review> reviews = new ArrayList<>();
+        
+        
+        when(reviewRepo.findAll()).thenReturn(reviews);
+
+        assertEquals(0, reviewRepo.findAll().size());
+
+    }
+    
+    @Test
+    public void saveTicket() {
+
+        Ticket ticket = new Ticket();
+        
+        ticket.setCustomerEmail("customer@gmail.com");
+        ticket.setDescription("description");
+        ticket.setId(1L);
+        ticket.setPriority("Low");
+        
+        
+        
+        
+        List<Ticket> tickets = new ArrayList<>();
+        tickets.add(ticket);
+        
+        
+        when(ticketRepo.findAll()).thenReturn(tickets);
+
+        assertEquals(1, ticketRepo.findAll().size());
+
+    }
    
-    
-    
+    @Test
+    public void saveNoTicket() {
+
+        Ticket ticket = new Ticket();
+        
+        ticket.setCustomerEmail("customer@gmail.com");
+        ticket.setDescription("description");
+        ticket.setId(1L);
+        ticket.setPriority("Low");
+        
+        
+        
+        
+        List<Ticket> tickets = new ArrayList<>();
+        
+        
+        when(ticketRepo.findAll()).thenReturn(tickets);
+
+        assertEquals(0, ticketRepo.findAll().size());
+
+    }
 
 }
